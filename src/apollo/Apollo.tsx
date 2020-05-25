4e49d9f9c722a627f0633591b17a865d99790ba8
biro.vince <quick.biro@gmail.com>
2020-05-25 19:29:10 +0200

    Production

--- src/apollo/Apollo.tsx
+++ src/apollo/Apollo.tsx
@@ -3,8 +3,6 @@ import { HttpLink } from "apollo-link-http";
 import { ApolloLink } from "apollo-link";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-//const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
-
 const authMiddleware = new ApolloLink((operation: any, forward: any) => {
   operation.setContext({
     headers: {
@@ -14,13 +12,10 @@ const authMiddleware = new ApolloLink((operation: any, forward: any) => {
   return forward(operation);
 });
 
-//const ssrMode = typeof window === 'undefined'
 const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
-  //ssrMode,
   link: ann(),
-  //link: httpLink,
   cache,
 });
 
@@ -30,6 +25,6 @@ function ann() {
 
 function createIsomorphLink() {
   return new HttpLink({
-    uri: "http://127.0.0.1:4000",
+    uri: "https://prisma.biro.wtf",
   });
 }
